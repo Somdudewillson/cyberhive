bc52a40b8072bee34ec21a925df611fb060983bf
Somdudewillson <somdudewilson@gmail.com>
2024-09-02 18:53:27 -0700

    Made internal nanites clear most negative potion effects

--- src/main/java/somdudewillson/cyberhive/common/effect/NaniteConvertEffect.java
+++ src/main/java/somdudewillson/cyberhive/common/effect/NaniteConvertEffect.java
@@ -1,12 +1,16 @@
 package somdudewillson.cyberhive.common.effect;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Fluids;
 import somdudewillson.cyberhive.common.CyberBlocks;
@@ -24,8 +28,15 @@ public class NaniteConvertEffect extends MobEffect {
     public void applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
 		pLivingEntity.hurt(CyberDamageTypes.makeSourceInternalNanites(pLivingEntity.level()), 2F);
 		
-        if (!pLivingEntity.isAlive()
-        		&& !pLivingEntity.level().isClientSide()) {
+		if (pLivingEntity.level().isClientSide()) { return; }
+		
+		pLivingEntity.getActiveEffectsMap().entrySet().stream()
+			.filter(e->!e.getKey().isBeneficial())
+			.filter(e->!e.getKey().isInstantenous())
+			.filter(e->e.getValue().isCurativeItem(new ItemStack(Items.MILK_BUCKET)))
+			.forEach(e->pLivingEntity.removeEffect(e.getKey()));
+		
+        if (!pLivingEntity.isAlive()) {
         	Optional<BlockPos> spawnPos = BlockPos.findClosestMatch(
         			pLivingEntity.blockPosition(), 3, 15, 
         			p -> {
@@ -51,4 +62,9 @@ public class NaniteConvertEffect extends MobEffect {
 		int interval = 15>>pAmplifier;
 		return (pDuration & interval) == 0;
 	}
+	
+	@Override
+    public List<ItemStack> getCurativeItems() {
+        return new ArrayList<ItemStack>();
+    }
 }
